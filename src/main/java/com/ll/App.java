8260e19521bccc3c3303ec71b0c4d71335d290f0
seolheun36 <seolheun36@gmail.com>
2025-05-03 14:17:18 +0900

    Todo 객체를 리스트에 담기

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -7,19 +9,22 @@ public class App {
         System.out.println("할 일 관리 앱 시작");
 
         try (Scanner scanner = new Scanner(System.in)) {
+            List<Todo> todos = new ArrayList<>();
+            long todosLastID = 0;
+
             while (true) {
                 System.out.print("명령) ");
                 String cmd = scanner.nextLine().trim();
 
                 if (cmd.equals("exit")) break;
                 else if (cmd.equals("add")) {
-                    long id = 1;
+                    long id = todosLastID + 1;
                     System.out.print("할 일: ");
                     String content = scanner.nextLine().trim();
 
                     Todo todo = new Todo(id, content);
-
-                    System.out.println(todo.getId());
+                    todos.add(todo);
+                    todosLastID++;
 
                     System.out.printf("%d번 할 일일 생성되었습니다.\n", id);
                 }
