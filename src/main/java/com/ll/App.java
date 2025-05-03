8874ad9acc4ff66f398c92b05e658475bed507ec
seolheun36 <seolheun36@gmail.com>
2025-05-03 16:00:29 +0900

    feat: modify 기능 구현

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -43,6 +43,25 @@ public class App {
                     }
 
                     System.out.printf("%d번 할 일이 삭제되었습니다.\n", id);
+                } else if (cmd.equals("modify")) {
+                    System.out.print("수정할 할 일의 번호: ");
+                    long id = Long.parseLong(scanner.nextLine().trim());
+
+                    Todo foundTodo = todos.stream()
+                            .filter(t -> t.getId() == id)
+                            .findFirst()
+                            .orElse(null);
+
+                    if (foundTodo == null) {
+                        System.out.printf("%d번 할 일은 존재하지 않습니다.\n", id);
+                        continue;
+                    }
+
+                    System.out.printf("기존 할 일: %s\n", foundTodo.getContent());
+                    System.out.print("새 할 일: ");
+                    foundTodo.setContent(scanner.nextLine().trim());
+
+                    System.out.printf("%d번 할 일이 수정되었습니다.\n", id);
                 }
             }
         }
